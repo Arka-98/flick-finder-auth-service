00f9d90a64b14297eea36e22b2f99490d692dbfd
Arkadipta <dasarkadipta@gmail.com>
2025-05-31 12:17:26 +0530

    fix: minor bug

--- src/auth/schemas/user.schema.ts
+++ src/auth/schemas/user.schema.ts
@@ -1,4 +1,4 @@
-import { IUser, RolesEnum } from '@flick-finder/common';
+import { RolesEnum } from '@flick-finder/common';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
@@ -12,7 +12,7 @@ export type UserDocument = HydratedDocument<User>;
     },
   },
 })
-export class User implements IUser {
+export class User {
   @Prop()
   name: string;
 

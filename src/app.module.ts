5921b5c2fc15143902645f13d380a2446f140cf2
Arkadipta <dasarkadipta@gmail.com>
2025-09-06 22:59:36 +0530

    fix: upgrade flick finder common lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
@@ -11,17 +9,15 @@ import { CommonModule, LoggerMiddleware } from '@flick-finder/common';
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGO_URI'),
         dbName: configService.get('MONGO_DB_NAME'),
       }),
       inject: [ConfigService],
     }),
     AuthModule,
-    CommonModule,
+    CommonModule.register(),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
